698edee8bc1604230652b6494533e9258dc6f646
Thomas K <thomas.katzlberger@locandy.com>
2013-06-08 08:54:22 +0200

    deprecated nsstring extensions

---
+++ BButton/FontAwesomeIcons.h
@@ -0,0 +1,14 @@
+//
+//  FontAwesomeIcons.h
+//  BButtonDemo
+//
+//  Created by Thomas on 08.06.13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#ifndef BButtonDemo_FontAwesomeIcons_h
+#define BButtonDemo_FontAwesomeIcons_h
+
+
+
+#endif
